a39de2542b598527e5559eb7fcd3cfff43fb8c2f
Marek Gacek <marekgacek45@gmail.com>
2024-12-11 15:46:27 +0100

    create form to create pitch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,76 +1,76 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import "easymde/dist/easymde.min.css";
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import './globals.css'
+import 'easymde/dist/easymde.min.css'
+import { Toaster } from '@/components/ui/toaster'
 
 const workSans = localFont({
-  src:[
-    {
-      path:'./fonts/WorkSans-Black.ttf',
-      weight:'900',
-      style:'normal'
-    },
-    {
-      path:'./fonts/WorkSans-ExtraBold.ttf',
-      weight:'800',
-      style:'normal'
-    },
-    {
-      path:'./fonts/WorkSans-Bold.ttf',
-      weight:'700',
-      style:'normal'
-    },
-    {
-      path:'./fonts/WorkSans-SemiBold.ttf',
-      weight:'600',
-      style:'normal'
-    },
-    {
-      path:'./fonts/WorkSans-Medium.ttf',
-      weight:'500',
-      style:'normal'
-    },
-    {
-      path:'./fonts/WorkSans-Regular.ttf',
-      weight:'400',
-      style:'normal'
-    },
-    {
-      path:'./fonts/WorkSans-Thin.ttf',
-      weight:'300',
-      style:'normal'
-    },
-    {
-      path:'./fonts/WorkSans-Light.ttf',
-      weight:'200',
-      style:'normal'
-    },
-    {
-      path:'./fonts/WorkSans-ExtraLight.ttf',
-      weight:'100',
-      style:'normal'
-    },
-  ],
-  variable:'--font-work-sans'
+	src: [
+		{
+			path: './fonts/WorkSans-Black.ttf',
+			weight: '900',
+			style: 'normal',
+		},
+		{
+			path: './fonts/WorkSans-ExtraBold.ttf',
+			weight: '800',
+			style: 'normal',
+		},
+		{
+			path: './fonts/WorkSans-Bold.ttf',
+			weight: '700',
+			style: 'normal',
+		},
+		{
+			path: './fonts/WorkSans-SemiBold.ttf',
+			weight: '600',
+			style: 'normal',
+		},
+		{
+			path: './fonts/WorkSans-Medium.ttf',
+			weight: '500',
+			style: 'normal',
+		},
+		{
+			path: './fonts/WorkSans-Regular.ttf',
+			weight: '400',
+			style: 'normal',
+		},
+		{
+			path: './fonts/WorkSans-Thin.ttf',
+			weight: '300',
+			style: 'normal',
+		},
+		{
+			path: './fonts/WorkSans-Light.ttf',
+			weight: '200',
+			style: 'normal',
+		},
+		{
+			path: './fonts/WorkSans-ExtraLight.ttf',
+			weight: '100',
+			style: 'normal',
+		},
+	],
+	variable: '--font-work-sans',
 })
 
 export const metadata: Metadata = {
-  title: "StartUp Platform",
-  description: "Meta desc",
-};
+	title: 'StartUp Platform',
+	description: 'Meta desc',
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body 
-        className={`${workSans.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={`${workSans.variable} antialiased`}>
+				{children}
+				<Toaster />
+			</body>
+		</html>
+	)
 }
